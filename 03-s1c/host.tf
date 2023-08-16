82eece530907fac3a279814c890dd57efd30f09b
midec82 <142382373+midec82@users.noreply.github.com>
2023-08-16 08:10:49 +0000

    host & publish

--- 03-s1c/host.tf
+++ 03-s1c/host.tf
@@ -1,6 +1,6 @@
 resource "checkpoint_management_host" "example" {
   name         = "martin"
   ipv4_address = "192.0.2.77"
-  color = "red"
+  color = "orange"
   tags = ["made by tf"]
 }
--- 03-s1c/publish.tf
+++ 03-s1c/publish.tf
@@ -7,5 +7,5 @@ variable "publish" {
 resource "checkpoint_management_publish" "example" {
   count    = var.publish ? 1 : 0
   triggers = ["${timestamp()}"]
-
+  depends_on = [ checkpoint_management_host.example ]
 }
